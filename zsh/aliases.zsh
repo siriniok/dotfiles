70654c69b2ea335daddd284f9a69e0d0d1e15016
Vladyslav Siriniok <siriniok@gmail.com>
2016-10-21 01:06:08 +0300

    Add some useful aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,9 @@
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
 
+# Enable aliases to be sudo’ed
+#   http://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo
+alias sudo='sudo '
 
 # Zsh
 
@@ -76,14 +79,36 @@ alias currentdate='date "+%m/%d/%Y"'
 alias h="history"
 alias hosts="$EDITOR /etc/hosts"
 alias ip="curl ipinfo.io/ip"
-alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
-alias copy="xclip -sel clip"
+alias localips='ifconfig | sed -En "s/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p"'
 alias my-ssh="xclip -sel clip < ~/.ssh/id_rsa.pub"
 alias weather="curl -4 http://wttr.in"
 alias stest="wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip"
 
+# Show $PATH in readable view
+alias path='echo -e ${PATH//:/\\n}'
 
-# Directories
+# Download web page with all assets
+alias getpage='wget --no-clobber --page-requisites --html-extension --convert-links --no-host-directories'
 
-alias dotfiles="cd $DOTFILES"
-alias wd="cd ~/code"
+# Download file with original filename
+alias get="curl -O -L"
+
+# Clipboard tools
+if [ command -v xclip >/dev/null 2>&1 ]; then
+  alias xcopy='xclip -selection clipboard'
+  alias xpaste='xclip -selection clipboard -o'
+fi
+
+# Password generator
+# Gemnerate random password, copies it into clipboard and outputs it to terminal
+if [ command -v pbcopy >/dev/null 2>&1 ]; then
+  alias password='openssl rand -base64 ${1:-9} | pbcopy ; echo "$(pbpaste)"'
+fi
+if [ command -v xcopy >/dev/null 2>&1 ]; then
+  alias password='openssl rand -base64 ${1:-9} | xcopy ; echo "$(xpaste)"'
+fi
+
+
+# Folders Shortcuts
+[ -d $DOTFILES ]              && alias dotfiles="cd $DOTFILES"
+[ -d ~/code ]                 && alias dl='cd ~/code'
