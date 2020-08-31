4acbf31f1cbc0ab86f8f631664b835e4885f11c6
Vladyslav Siriniok <siriniok@gmail.com>
2020-08-31 11:42:12 +0300

    vscode: add configs

---
+++ setup/vscode-plugins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Visual Studio Code :: Package list
+plugins=(
+  ms-vsliveshare.vsliveshare
+  vscodevim.vim
+)
+
+for plugin in ${plugins[@]}; do
+  code --install-extension $plugin
+done
