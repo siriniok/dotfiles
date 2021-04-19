22f4dcba7ba08108add3cc6e8c325eed12f3b796
Vladyslav Siriniok <siriniok@gmail.com>
2021-04-19 21:46:27 +0300

    setup: add new vscode plugins

--- setup/vscode-plugins.sh
+++ setup/vscode-plugins.sh
@@ -6,6 +6,18 @@ plugins=(
   pnp.polacode
   vscodevim.vim
   bradlc.vscode-tailwindcss
+  streetsidesoftware.code-spell-checker-ukrainian
+  timonwong.shellcheck
+  mechatroner.rainbow-csv
+  ryu1kn.partial-diff
+  howardzuo.vscode-npm-dependency
+  DavidAnson.vscode-markdownlint
+  yzhang.markdown-all-in-one
+  eamodio.gitlens
+  felipecaputo.git-project-manager
+  dbaeumer.vscode-eslint
+  EditorConfig.EditorConfig
+  streetsidesoftware.code-spell-checker
 )
 
 for plugin in ${plugins[@]}; do
